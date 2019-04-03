e59268f702f377decbe16e15f0c52c701438f77a
AlishaKhov <alishakhov@gmail.com>
2019-04-02 18:25:56 -0700

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,36 @@
+let btn = document.getElementById("btn"),
+    inputHeight = document.getElementById("inputHeight"),
+    inputWeight = document.getElementById("inputWeight"),
+    pixelCanvas = document.getElementById("pixelCanvas"),
+    table = document.querySelector("table"),
+    colorPicker = document.getElementById("colorPicker");
+
+function init() {
+  btn.addEventListener("click", function(event) {
+  event.preventDefault();
+  const getHeightValue = inputHeight.value;
+  const getWeightValue = inputWeight.value;
+  // Did not work event.clear();
+  makeGrid(getHeightValue, getWeightValue);
+});
+}
+
+init();
+
+//set the tablepixelCanvas
+function makeGrid(height, width) {
+  for (let i = 0; i < height; i++) {
+    let tr = document.createElement("TR");
+        table.appendChild(tr);
+
+    for(let t = 0; t < width; t++) {
+      let td = document.createElement("TD");
+          tr.appendChild(td);
+
+          td.addEventListener("click", function (event) {
+                td.style.backgroundColor = colorPicker.value;
+          });
+    }
+
+  }
+}
